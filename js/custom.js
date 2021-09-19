8b3a971489612dbf81d380c69e65d6a114e17ddd
Nisha <66770907+Nisha123-garg@users.noreply.github.com>
2021-09-19 20:46:37 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,40 @@
+//slick slider
+
+$('.slick_slider').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
